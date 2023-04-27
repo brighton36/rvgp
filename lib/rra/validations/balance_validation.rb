8ee40c6c868c092c8c83b0f3bfa6a79de6c18b08
Chris DeRose <cderose@derosetechnologies.com>
2023-04-27 15:19:30 -0400

    Something broke the BalanceValidation in ledger 3.3.2 . Switched to the hledger balance implementation, rather than fixing the ledger issue

--- lib/rra/validations/balance_validation.rb
+++ lib/rra/validations/balance_validation.rb
@@ -1,41 +1,48 @@
+# frozen_string_literal: true
+
+# This validation asserts that the ledger-reported balance, matches a provided
+# balance, on a given day. Presumably, this provisional balance, comes from a
+# bank statement.
 class BalanceValidation < RRA::JournalValidationBase
   def validate
-    if transformer.balances.nil? || transformer.balances.length == 0
-      warning! "No balance checkpoints found."
+    if transformer.balances.nil? || transformer.balances.empty?
+      warning! 'No balance checkpoints found.'
     else
       is_account_valid = true
-      cite_balances = transformer.balances.collect{|d, expected_balance_s| 
+      cite_balances = transformer.balances.map do |d, expected_balance_s|
         expected_balance = expected_balance_s.to_commodity
 
-        balances_on_day = RRA::Ledger.balance transformer.from, depth: 1, end: d.to_s
-        
-        balances_found = balances_on_day.accounts.collect(&:amounts).flatten.find_all{|amount|
-          amount.code == expected_balance.code }
+        balances_on_day = RRA::HLedger.balance transformer.from, depth: 1, end: d.to_s
 
-        found = if balances_found.length == 0
-          # Rather than operate from nil, we'll establish that we're '0' of units
-          # of the expected symbol
-          RRA::Journal::Commodity.from_symbol_and_amount expected_balance.code, 0
-        else
-          balances_found.sum
+        balances_found = balances_on_day.accounts.map(&:amounts).flatten.find_all do |amount|
+          amount.code == expected_balance.code
         end
 
-        if found
-          found_as_s = "Found: %s" % found.to_s
-        else
-          found_as_s = '(Nil)'
-          found = RRA::Journal::Commodity.from_symbol_and_amount expected_balance.code, 0
-        end
+        found = if balances_found.empty?
+                  # Rather than operate from nil, we'll establish that we're '0' of units
+                  # of the expected symbol
+                  RRA::Journal::Commodity.from_symbol_and_amount expected_balance.code, 0
+                else
+                  balances_found.sum
+                end
+
+        found_as_s = if found
+                       format('Found: %s', found.to_s)
+                     else
+                       found = RRA::Journal::Commodity.from_symbol_and_amount expected_balance.code, 0
+                       '(Nil)'
+                     end
 
         is_valid = expected_balance == found
         is_account_valid = false unless is_valid
-        
-        "(%s) Expected: %s %s" % [ d.to_s, expected_balance.to_s,
-          RRA.pastel.send( (is_valid) ? :green : :red, found_as_s ) ]
-      }
 
-      error! "Failed Checkpoint(s):", cite_balances unless is_account_valid
+        format('(%<day>s) Expected: %<expected>s %<indicator>s',
+               day: d.to_s,
+               expected: expected_balance.to_s,
+               indicator: RRA.pastel.send(is_valid ? :green : :red, found_as_s))
+      end
+
+      error! 'Failed Checkpoint(s):', cite_balances unless is_account_valid
     end
   end
 end
-
